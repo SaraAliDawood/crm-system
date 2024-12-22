f7d31bd61431885b2a6dc43993e208d3cb3cc7a9
SaraAliDawood <saradawood605@gmail.com>
2024-12-22 13:39:26 +0200

    Deploy build to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: './', // Ensure this matches your project structure
+  base: '/crm-system/', // This should match the repository name for GitHub Pages
 });
